1a8e85afa9d6d9aec471a6383509ea670719b669
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-03-16 00:03:53 -0300

    to-do list com scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+//Pages
+import { HomeComponent } from "./modules/home/pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{ }
